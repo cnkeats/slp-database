27a0ca15bb36cf66990ba80e4910ad2da4d06862
Calvin <calvin.keats@gmail.com>
2020-11-22 15:22:35 -0600

    Added .NET Core project

---
+++ SlippiStats/SlippiStats/Controllers/RequestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SlippiStats.Controllers
+{
+    public class RequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MetaStats()
+        {
+            return View();
+        }
+    }
+}
---
+++ SlippiStats/SlippiStats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SlippiStats.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

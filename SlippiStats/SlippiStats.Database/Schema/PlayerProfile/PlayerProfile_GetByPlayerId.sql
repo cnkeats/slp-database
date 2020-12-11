dacf4523b05444141d398b55198efb4548c99d89
Calvin <calvin.keats@gmail.com>
2020-12-11 11:55:44 -0600

    Unhid Player Profile fields

--- SlippiStats/SlippiStats.Database/Schema/PlayerProfile/PlayerProfile_GetByPlayerId.sql
+++ SlippiStats/SlippiStats.Database/Schema/PlayerProfile/PlayerProfile_GetByPlayerId.sql
@@ -6,7 +6,6 @@
 
 AS
 
-
 SELECT
 	Player.Id,
 	Player.Name,
@@ -14,13 +13,213 @@ SELECT
 	SUM(CASE WHEN Player1Id = @playerId THEN CAST(Player1Victory AS INT) ELSE CAST(Player2Victory AS INT) END) AS GamesWon,
 	SUM(Game.GameLength) AS FramesPlayed,
 	SUM(Game.GameLength) / COUNT(Game.Id) / 60 AS AverageGameDuration,
-	1 AS FavoriteCharacter,
-	1 AS FavoriteOpponent,
-	CAST('3030-06-30' AS DATETIME) AS FirstSpotted,
-	-1 AS UniqueOpponents,
-	-1 AS FourStocks,
-	-1 AS LRASCount,
-	-1 AS OpponentLRASCount,
+	(
+		SELECT TOP 1
+			Character.Id
+		FROM
+			Player WITH (NOLOCK)
+			INNER JOIN Game WITH (NOLOCK)
+				ON Game.Player1Id = Player.Id
+				OR Game.Player2Id = Player.Id
+			INNER JOIN Character WITH (NOLOCK)
+				ON (Character.Id = Game.Character1 AND Player.Id = Game.Player1Id)
+				OR (Character.Id = Game.Character2 AND Player.Id = Game.Player2Id)
+			INNER JOIN Player Opponent WITH (NOLOCK)
+				ON (Opponent.Id = Game.Player1Id AND Game.Player1Id <> @playerId)
+				OR (Opponent.Id = Game.Player2Id AND Game.Player2Id <> @playerId)
+		WHERE
+			Player.Id = @playerId
+			AND (@opponentFilter IS NULL OR Opponent.Name LIKE @opponentFilter OR Opponent.ConnectCode LIKE @opponentFilter)
+			AND (
+				@characterFilter IS NULL
+				OR (Game.Character1 = @characterFilter AND Game.Player1Id = Player.Id)
+				OR (Game.Character2 = @characterFilter AND Game.Player2Id = Player.Id)
+			)
+			AND (
+				@opponentCharacterFilter IS NULL
+				OR (Game.Character1 = @opponentCharacterFilter AND Game.Player1Id <> Player.Id)
+				OR (Game.Character2 = @opponentCharacterFilter AND Game.Player2Id <> Player.Id)
+			)
+		GROUP BY
+			Character.Id
+		ORDER BY
+			COUNT(*) DESC
+	) AS FavoriteCharacter,
+	(
+		SELECT TOP 1
+			Opponent.Id
+		FROM
+			Player WITH (NOLOCK)
+			INNER JOIN Game WITH (NOLOCK)
+				ON Game.Player1Id = Player.Id
+				OR Game.Player2Id = Player.Id
+			INNER JOIN Player Opponent WITH (NOLOCK)
+				ON (Opponent.Id = Game.Player1Id AND Game.Player1Id <> @playerId)
+				OR (Opponent.Id = Game.Player2Id AND Game.Player2Id <> @playerId)
+		WHERE
+			Player.Id = @playerId
+			AND (@opponentFilter IS NULL OR Opponent.Name LIKE @opponentFilter OR Opponent.ConnectCode LIKE @opponentFilter)
+			AND (
+				@characterFilter IS NULL
+				OR (Game.Character1 = @characterFilter AND Game.Player1Id = Player.Id)
+				OR (Game.Character2 = @characterFilter AND Game.Player2Id = Player.Id)
+			)
+			AND (
+				@opponentCharacterFilter IS NULL
+				OR (Game.Character1 = @opponentCharacterFilter AND Game.Player1Id <> Player.Id)
+				OR (Game.Character2 = @opponentCharacterFilter AND Game.Player2Id <> Player.Id)
+			)
+		GROUP BY
+			Opponent.Id
+		ORDER BY
+			COUNT(*) DESC
+	) AS FavoriteOpponent,
+	(
+		SELECT TOP 1
+			StartAt
+		FROM
+			Player WITH (NOLOCK)
+			INNER JOIN Game WITH (NOLOCK)
+				ON Game.Player1Id = Player.Id
+				OR Game.Player2Id = Player.Id
+			INNER JOIN Player Opponent WITH (NOLOCK)
+				ON (Opponent.Id = Game.Player1Id AND Game.Player1Id <> @playerId)
+				OR (Opponent.Id = Game.Player2Id AND Game.Player2Id <> @playerId)
+		WHERE
+			Player.Id = @playerId
+			AND (@opponentFilter IS NULL OR Opponent.Name LIKE @opponentFilter OR Opponent.ConnectCode LIKE @opponentFilter)
+			AND (
+				@characterFilter IS NULL
+				OR (Game.Character1 = @characterFilter AND Game.Player1Id = Player.Id)
+				OR (Game.Character2 = @characterFilter AND Game.Player2Id = Player.Id)
+			)
+			AND (
+				@opponentCharacterFilter IS NULL
+				OR (Game.Character1 = @opponentCharacterFilter AND Game.Player1Id <> Player.Id)
+				OR (Game.Character2 = @opponentCharacterFilter AND Game.Player2Id <> Player.Id)
+			)
+		ORDER BY
+			Game.StartAt
+	) AS FirstSpotted,
+	(
+		SELECT
+			COUNT(U.Id)
+		FROM
+			(SELECT DISTINCT
+				Opponent.Id
+			FROM
+				Player WITH (NOLOCK)
+				INNER JOIN Game WITH (NOLOCK)
+					ON Game.Player1Id = Player.Id
+					OR Game.Player2Id = Player.Id
+				INNER JOIN Player Opponent WITH (NOLOCK)
+					ON (Opponent.Id = Game.Player1Id AND Game.Player1Id <> @playerId)
+					OR (Opponent.Id = Game.Player2Id AND Game.Player2Id <> @playerId)
+			WHERE
+				Player.Id = @playerId
+				AND (@opponentFilter IS NULL OR Opponent.Name LIKE @opponentFilter OR Opponent.ConnectCode LIKE @opponentFilter)
+				AND (
+					@characterFilter IS NULL
+					OR (Game.Character1 = @characterFilter AND Game.Player1Id = Player.Id)
+					OR (Game.Character2 = @characterFilter AND Game.Player2Id = Player.Id)
+				)
+				AND (
+					@opponentCharacterFilter IS NULL
+					OR (Game.Character1 = @opponentCharacterFilter AND Game.Player1Id <> Player.Id)
+					OR (Game.Character2 = @opponentCharacterFilter AND Game.Player2Id <> Player.Id)
+				)
+			) AS U
+	) AS UniqueOpponents,
+	(
+		SELECT
+			COUNT(*)
+		FROM
+			Player WITH (NOLOCK)
+			INNER JOIN Game WITH (NOLOCK)
+				ON (Game.Player1Id = @playerId
+					AND Game.Player1Victory = 1
+						AND Game.Player1EndingStocks = 4
+					)
+				OR (Game.Player2Id = @playerId
+					AND Game.Player2Victory = 1
+					AND Game.Player2EndingStocks = 4
+				)
+			INNER JOIN Player Opponent WITH (NOLOCK)
+				ON (Opponent.Id = Game.Player1Id AND Game.Player1Id <> @playerId)
+				OR (Opponent.Id = Game.Player2Id AND Game.Player2Id <> @playerId)
+		WHERE
+			Player.Id = @playerId
+			AND Game.LRASInitiatorIndex = -1
+			AND (@opponentFilter IS NULL OR Opponent.Name LIKE @opponentFilter OR Opponent.ConnectCode LIKE @opponentFilter)
+			AND (
+				@characterFilter IS NULL
+				OR (Game.Character1 = @characterFilter AND Game.Player1Id = Player.Id)
+				OR (Game.Character2 = @characterFilter AND Game.Player2Id = Player.Id)
+			)
+			AND (
+				@opponentCharacterFilter IS NULL
+				OR (Game.Character1 = @opponentCharacterFilter AND Game.Player1Id <> Player.Id)
+				OR (Game.Character2 = @opponentCharacterFilter AND Game.Player2Id <> Player.Id)
+			)
+	) AS FourStocks,
+	(
+		SELECT
+			COUNT(*)
+		FROM
+			Player WITH (NOLOCK)
+			INNER JOIN Game WITH (NOLOCK)
+				ON (Game.Player1Id = @playerId
+					AND LRASInitiatorIndex = 0
+					)
+				OR (Game.Player2Id = @playerId
+					AND LRASInitiatorIndex = 1
+				)
+			INNER JOIN Player Opponent WITH (NOLOCK)
+				ON (Opponent.Id = Game.Player1Id AND Game.Player1Id <> @playerId)
+				OR (Opponent.Id = Game.Player2Id AND Game.Player2Id <> @playerId)
+		WHERE
+			Player.Id = @playerId
+			AND (@opponentFilter IS NULL OR Opponent.Name LIKE @opponentFilter OR Opponent.ConnectCode LIKE @opponentFilter)
+			AND (
+				@characterFilter IS NULL
+				OR (Game.Character1 = @characterFilter AND Game.Player1Id = Player.Id)
+				OR (Game.Character2 = @characterFilter AND Game.Player2Id = Player.Id)
+			)
+			AND (
+				@opponentCharacterFilter IS NULL
+				OR (Game.Character1 = @opponentCharacterFilter AND Game.Player1Id <> Player.Id)
+				OR (Game.Character2 = @opponentCharacterFilter AND Game.Player2Id <> Player.Id)
+			)
+	) AS LRASCount,
+	(
+		SELECT
+			COUNT(*)
+		FROM
+			Player WITH (NOLOCK)
+			INNER JOIN Game WITH (NOLOCK)
+				ON (Game.Player1Id = @playerId
+					AND LRASInitiatorIndex = 1
+					)
+				OR (Game.Player2Id = @playerId
+					AND LRASInitiatorIndex = 0
+				)
+			INNER JOIN Player Opponent WITH (NOLOCK)
+				ON (Opponent.Id = Game.Player1Id AND Game.Player1Id <> @playerId)
+				OR (Opponent.Id = Game.Player2Id AND Game.Player2Id <> @playerId)
+		WHERE
+			Player.Id = @playerId
+			AND (@opponentFilter IS NULL OR Opponent.Name LIKE @opponentFilter OR Opponent.ConnectCode LIKE @opponentFilter)
+			AND (
+				@characterFilter IS NULL
+				OR (Game.Character1 = @characterFilter AND Game.Player1Id = Player.Id)
+				OR (Game.Character2 = @characterFilter AND Game.Player2Id = Player.Id)
+			)
+			AND (
+				@opponentCharacterFilter IS NULL
+				OR (Game.Character1 = @opponentCharacterFilter AND Game.Player1Id <> Player.Id)
+				OR (Game.Character2 = @opponentCharacterFilter AND Game.Player2Id <> Player.Id)
+			)
+	) AS OpponentLRASCount,
 	@characterFilter AS CharacterFilter,
 	@opponentCharacterFilter AS OpponentCharacterFilter,
 	@opponentFilter AS OpponentFilter
